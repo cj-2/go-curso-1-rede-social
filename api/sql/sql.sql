13d4997fc45972dd6e57e044e9585881a4eea81e
Carlos Júnior <crpu4a@gmail.com>
2026-07-06 18:54:55 -0300

    featT: adição de modelo Usuario e código sql de utilitário.

---
+++ api/sql/sql.sql
@@ -0,0 +1,22 @@
+-- create database devbook;
+-- use devbook;
+-- create table usuarios (id int auto_increment primary key, nome varchar(50) not null, email varchar(50) not null) engine=INNODB;
+-- show tables;
+-- create user 'golang'@'%' identified by 'golang'; # golang é a senha
+
+-- # liberanto todos os privilégios para um usuário
+-- grant all privileges on devbook.* to 'golang'@'%';
+
+create database if not exists devbook;
+use devbook;
+
+drop table if exists usuarios;
+
+create table usuarios(
+    id int auto_increment primary key,
+    nome varchar(50) not null,
+    nick varchar(50) not null unique,
+    email varchar(50) not null unique,
+    senha varchar(50) not null,
+    criadoEm timestamp default current_timestamp()
+) ENGINE = INNODB;
