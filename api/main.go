60244d88b57cfc6ca96fe479eaaecf209bf0df5f
Carlos Júnior <crpu4a@gmail.com>
2026-07-07 08:30:37 -0300

    configuração de variáveis de ambiente

--- api/main.go
+++ api/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/router"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	config.Carregar()
+	fmt.Println("Iniciando API...")
+	// fmt.Println(config.Porta, config.StringConexaoBanco)
+	fmt.Printf("Escutando na porta %d \n", config.Porta)
 	r := router.Gerar()
-	log.Fatal(http.ListenAndServe(":5000", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }
