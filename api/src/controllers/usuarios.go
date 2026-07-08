712607c35d9b8f743fade30fc809fb8865adbbb8
Carlos Júnior <crpu4a@gmail.com>
2026-07-08 08:35:00 -0300

    criação de métodos de resposta e repositório de criar usuário

--- api/src/controllers/usuarios.go
+++ api/src/controllers/usuarios.go
@@ -4,40 +4,44 @@ import (
 	"api/src/banco"
 	"api/src/modelos"
 	"api/src/repositorios"
+	"api/src/respostas"
 	"encoding/json"
-	"fmt"
 	"io"
-	"log"
 	"net/http"
 )
 
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	body, erro := io.ReadAll(r.Body)
 	if erro != nil {
-		log.Fatal(erro)
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
 	}
 
 	// Copiando o conteúdo de body para usuário.
 	var usuario modelos.Usuario
 	if erro = json.Unmarshal(body, &usuario); erro != nil {
-		log.Fatal(erro)
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
 	}
 
 	// Conexão com banco de dados
 	db, erro := banco.Conectar()
 	if erro != nil {
-		log.Fatal(erro)
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
 	}
+	defer db.Close()
 
 	// Criação do repositório:
 	repositorio := repositorios.NovoRespositorioDeUsuarios(db)
 
-	id, erro := repositorio.Criar(usuario)
+	usuario.Id, erro = repositorio.Criar(usuario)
 	if erro != nil {
-		log.Fatal(erro)
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
 	}
 
-	w.Write([]byte(fmt.Sprintf("Criando Usuário %d", id)))
+	respostas.JSON(w, http.StatusCreated, usuario)
 }
 
 func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
---
+++ api/src/respostas/respostas.go
@@ -0,0 +1,25 @@
+package respostas
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// Função responsável por formatar o resultado da API
+func JSON(w http.ResponseWriter, statusCode int, dados interface{}) {
+	w.WriteHeader(statusCode)
+
+	if erro := json.NewEncoder(w).Encode(dados); erro != nil {
+		log.Fatal(erro)
+	}
+}
+
+// Retorna a resposta de erro
+func Erro(w http.ResponseWriter, statusCode int, erro error) {
+	JSON(w, statusCode, struct {
+		Erro string `json:"erro"`
+	}{
+		Erro: erro.Error(),
+	})
+}
