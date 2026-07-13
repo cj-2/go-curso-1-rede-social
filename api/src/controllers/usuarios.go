b40d9b10bf5e072ff7dbfb0756bc190aba2596df
Carlos Júnior <crpu4a@gmail.com>
2026-07-13 09:12:42 -0300

    adição de busca de usuário por id

--- api/src/controllers/usuarios.go
+++ api/src/controllers/usuarios.go
@@ -8,7 +8,10 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"strconv"
 	"strings"
+
+	"github.com/gorilla/mux"
 )
 
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
@@ -72,7 +75,30 @@ func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
 }
 
 func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Buscando Usuário"))
+	paramanetros := mux.Vars(r)
+	usuarioId, erro := strconv.ParseUint(paramanetros["usuarioId"], 10, 64)
+
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRespositorioDeUsuarios(db)
+
+	usuario, erro := repositorio.BuscarPorId(usuarioId)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusOK, usuario)
 }
 
 func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
